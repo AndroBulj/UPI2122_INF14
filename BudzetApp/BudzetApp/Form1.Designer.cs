f9a2f94173d954656f62eea89aa61b9d30379a27
AndroBulj <abulj@pmfst.hr>
2021-12-13 20:39:38 +0100

    Pregled transakcija

--- BudzetApp/BudzetApp/Form1.Designer.cs
+++ BudzetApp/BudzetApp/Form1.Designer.cs
@@ -29,41 +29,43 @@ namespace BudzetApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rbtnPrihod = new System.Windows.Forms.RadioButton();
+            this.rbtnRashod = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblOpis = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.cmbValuta = new System.Windows.Forms.ComboBox();
+            this.txtBoxIznos = new System.Windows.Forms.TextBox();
+            this.txtBoxOpis = new System.Windows.Forms.TextBox();
+            this.rtbIspis = new System.Windows.Forms.RichTextBox();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnOdustani = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblUkupno = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // radioButton1
+            // rbtnPrihod
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(82, 83);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(73, 24);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Prihod";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.rbtnPrihod.AutoSize = true;
+            this.rbtnPrihod.Location = new System.Drawing.Point(82, 83);
+            this.rbtnPrihod.Name = "rbtnPrihod";
+            this.rbtnPrihod.Size = new System.Drawing.Size(73, 24);
+            this.rbtnPrihod.TabIndex = 0;
+            this.rbtnPrihod.TabStop = true;
+            this.rbtnPrihod.Text = "Prihod";
+            this.rbtnPrihod.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // rbtnRashod
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(82, 113);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(79, 24);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Rashod";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rbtnRashod.AutoSize = true;
+            this.rbtnRashod.Location = new System.Drawing.Point(82, 113);
+            this.rbtnRashod.Name = "rbtnRashod";
+            this.rbtnRashod.Size = new System.Drawing.Size(79, 24);
+            this.rbtnRashod.TabIndex = 1;
+            this.rbtnRashod.TabStop = true;
+            this.rbtnRashod.Text = "Rashod";
+            this.rbtnRashod.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -83,14 +85,14 @@ namespace BudzetApp
             this.label2.TabIndex = 3;
             this.label2.Text = "Iznos:";
             // 
-            // label3
+            // lblOpis
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(70, 162);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 20);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Opis:";
+            this.lblOpis.AutoSize = true;
+            this.lblOpis.Location = new System.Drawing.Point(70, 162);
+            this.lblOpis.Name = "lblOpis";
+            this.lblOpis.Size = new System.Drawing.Size(42, 20);
+            this.lblOpis.TabIndex = 4;
+            this.lblOpis.Text = "Opis:";
             // 
             // label4
             // 
@@ -101,81 +103,101 @@ namespace BudzetApp
             this.label4.TabIndex = 5;
             this.label4.Text = "Valuta:";
             // 
-            // comboBox1
+            // cmbValuta
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cmbValuta.FormattingEnabled = true;
+            this.cmbValuta.Items.AddRange(new object[] {
             "HRK",
             "EUR",
             "USD",
             "GBP"});
-            this.comboBox1.Location = new System.Drawing.Point(118, 256);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(151, 28);
-            this.comboBox1.TabIndex = 6;
-            this.comboBox1.Text = "HRK";
+            this.cmbValuta.Location = new System.Drawing.Point(118, 256);
+            this.cmbValuta.Name = "cmbValuta";
+            this.cmbValuta.Size = new System.Drawing.Size(151, 28);
+            this.cmbValuta.TabIndex = 6;
+            this.cmbValuta.Text = "HRK";
             // 
-            // textBox1
+            // txtBoxIznos
             // 
-            this.textBox1.Location = new System.Drawing.Point(118, 209);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(125, 27);
-            this.textBox1.TabIndex = 7;
+            this.txtBoxIznos.Location = new System.Drawing.Point(118, 209);
+            this.txtBoxIznos.Name = "txtBoxIznos";
+            this.txtBoxIznos.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxIznos.TabIndex = 7;
             // 
-            // textBox2
+            // txtBoxOpis
             // 
-            this.textBox2.Location = new System.Drawing.Point(118, 159);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(125, 27);
-            this.textBox2.TabIndex = 8;
+            this.txtBoxOpis.Location = new System.Drawing.Point(118, 159);
+            this.txtBoxOpis.Name = "txtBoxOpis";
+            this.txtBoxOpis.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxOpis.TabIndex = 8;
             // 
-            // richTextBox1
+            // rtbIspis
             // 
-            this.richTextBox1.Enabled = false;
-            this.richTextBox1.Location = new System.Drawing.Point(335, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(429, 387);
-            this.richTextBox1.TabIndex = 9;
-            this.richTextBox1.Text = "";
+            this.rtbIspis.Enabled = false;
+            this.rtbIspis.Location = new System.Drawing.Point(335, 12);
+            this.rtbIspis.Name = "rtbIspis";
+            this.rtbIspis.Size = new System.Drawing.Size(429, 387);
+            this.rtbIspis.TabIndex = 9;
+            this.rtbIspis.Text = "";
             // 
-            // button1
+            // btnDodaj
             // 
-            this.button1.Location = new System.Drawing.Point(59, 319);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Dodaj";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnDodaj.Location = new System.Drawing.Point(59, 319);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(94, 29);
+            this.btnDodaj.TabIndex = 10;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
             // 
-            // button2
+            // btnOdustani
             // 
-            this.button2.Location = new System.Drawing.Point(175, 319);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Odustani";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnOdustani.Location = new System.Drawing.Point(175, 319);
+            this.btnOdustani.Name = "btnOdustani";
+            this.btnOdustani.Size = new System.Drawing.Size(94, 29);
+            this.btnOdustani.TabIndex = 11;
+            this.btnOdustani.Text = "Odustani";
+            this.btnOdustani.UseVisualStyleBackColor = true;
+            this.btnOdustani.Click += new System.EventHandler(this.btnOdustani_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 415);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Ukupno:";
+            // 
+            // lblUkupno
+            // 
+            this.lblUkupno.AutoSize = true;
+            this.lblUkupno.Location = new System.Drawing.Point(404, 415);
+            this.lblUkupno.Name = "lblUkupno";
+            this.lblUkupno.Size = new System.Drawing.Size(0, 20);
+            this.lblUkupno.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 453);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblUkupno);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnOdustani);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.rtbIspis);
+            this.Controls.Add(this.txtBoxOpis);
+            this.Controls.Add(this.txtBoxIznos);
+            this.Controls.Add(this.cmbValuta);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblOpis);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rbtnRashod);
+            this.Controls.Add(this.rbtnPrihod);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BudgetApp";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,18 +205,20 @@ namespace BudzetApp
 
         #endregion
 
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rbtnPrihod;
+        private System.Windows.Forms.RadioButton rbtnRashod;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblOpis;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cmbValuta;
+        private System.Windows.Forms.TextBox txtBoxIznos;
+        private System.Windows.Forms.TextBox txtBoxOpis;
+        private System.Windows.Forms.RichTextBox rtbIspis;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnOdustani;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblUkupno;
     }
 }
 
--- BudzetApp/BudzetApp/Form1.cs
+++ BudzetApp/BudzetApp/Form1.cs
@@ -10,16 +10,109 @@ using System.Windows.Forms;
 
 namespace BudzetApp
 {
+    public class Transakcija
+    {
+        private string vrsta;
+        private string opis;
+        private double iznos = 0;
+        private string valuta;
+
+        public string Vrsta { get => vrsta; set => vrsta = value; }
+        public string Opis { get => opis; set => opis = value; }
+        public double Iznos { get => iznos; set => iznos = value; }
+        public string Valuta { get => valuta; set => valuta = value; }
+
+        public Transakcija(string _vrsta, string _opis, double _iznos, string _valuta)
+        {
+            this.Vrsta = _vrsta;
+            this.Opis = _opis;
+            this.Iznos = _iznos;
+            this.Valuta = _valuta;
+        }
+    }
+
     public partial class Form1 : Form
     {
+        private double total;
+
+        Dictionary<int, Transakcija> transakcije = new Dictionary<int, Transakcija>();
+
+        public double Total { get => total; set => total = value; }
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        private void btnDodaj_Click(object sender, EventArgs e)
         {
-            
+            string vrsta;
+            string opis;
+            double iznos;
+            string valuta;
+
+            if (rbtnPrihod.Checked)
+            {
+                vrsta = "Prihod";
+            }
+            else if (rbtnRashod.Checked)
+            {
+                vrsta = "Rashod";
+            }
+            else
+            {
+                MessageBox.Show("Niste odabrali vrstu prihoda.");
+                return;
+            }
+
+            opis = txtBoxOpis.Text;
+
+            try
+            {
+                iznos = Math.Round(double.Parse(txtBoxIznos.Text),2);
+            }
+            catch
+            {
+                MessageBox.Show("Krivi unos iznosa.");
+                return;
+            }
+
+            try
+            {
+                valuta = cmbValuta.SelectedItem.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Krivi odabir valute.");
+                return;
+            }
+
+            Transakcija nova = new Transakcija(vrsta, opis, iznos, valuta);
+
+            transakcije.Add(transakcije.Count, nova);
+
+            if (nova.Vrsta == "Prihod")
+            {
+                Total += nova.Iznos;
+            }
+            else
+            {
+                Total -= nova.Iznos;
+            }
+
+            rtbIspis.AppendText(nova.Vrsta + "\t " + nova.Opis + "\t " + nova.Iznos + "\t "+ nova.Valuta+"\n");
+            lblUkupno.Text = Total + " HRK";
+
+            rbtnPrihod.Checked = false;
+            rbtnRashod.Checked = false;
+            txtBoxOpis.Clear();
+            txtBoxIznos.Clear();
+        }
+
+        private void btnOdustani_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
