104aca4cc1959430ac2478f6d2e20d934d7e6560
AndroBulj <abulj@pmfst.hr>
2021-12-13 19:16:26 +0100

    Prvi dizajn

--- BudzetApp/BudzetApp/Form1.cs
+++ BudzetApp/BudzetApp/Form1.cs
@@ -16,5 +16,10 @@ namespace BudzetApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
