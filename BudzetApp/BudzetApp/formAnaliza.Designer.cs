810031ec03a14f801143c50e9d02e0f823472c63
Karlo Tomas <ktomas@pmfst.hr>
2022-01-16 19:43:58 +0100

    dodavanje/brisanje kategorija

--- BudzetApp/BudzetApp/formAnaliza.Designer.cs
+++ BudzetApp/BudzetApp/formAnaliza.Designer.cs
@@ -32,6 +32,13 @@ namespace BudzetApp
             this.rtbAnaliza = new System.Windows.Forms.RichTextBox();
             this.cmbValutaAnaliza = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.cmbKategorije = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbtnKatPrihodi = new System.Windows.Forms.RadioButton();
+            this.rbtnKatRashodi = new System.Windows.Forms.RadioButton();
+            this.btnIzbrisiKat = new System.Windows.Forms.Button();
+            this.btnDodajKat = new System.Windows.Forms.Button();
+            this.txtDodajKat = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // rtbAnaliza
@@ -65,11 +72,85 @@ namespace BudzetApp
             this.label1.TabIndex = 2;
             this.label1.Text = "Valuta:";
             // 
+            // cmbKategorije
+            // 
+            this.cmbKategorije.FormattingEnabled = true;
+            this.cmbKategorije.Location = new System.Drawing.Point(12, 357);
+            this.cmbKategorije.Name = "cmbKategorije";
+            this.cmbKategorije.Size = new System.Drawing.Size(121, 23);
+            this.cmbKategorije.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kategorije:";
+            // 
+            // rbtnKatPrihodi
+            // 
+            this.rbtnKatPrihodi.AutoSize = true;
+            this.rbtnKatPrihodi.Location = new System.Drawing.Point(12, 307);
+            this.rbtnKatPrihodi.Name = "rbtnKatPrihodi";
+            this.rbtnKatPrihodi.Size = new System.Drawing.Size(63, 19);
+            this.rbtnKatPrihodi.TabIndex = 5;
+            this.rbtnKatPrihodi.TabStop = true;
+            this.rbtnKatPrihodi.Text = "Prihodi";
+            this.rbtnKatPrihodi.UseVisualStyleBackColor = true;
+            this.rbtnKatPrihodi.CheckedChanged += new System.EventHandler(this.rbtnKatPrihodi_CheckedChanged);
+            // 
+            // rbtnKatRashodi
+            // 
+            this.rbtnKatRashodi.AutoSize = true;
+            this.rbtnKatRashodi.Location = new System.Drawing.Point(12, 332);
+            this.rbtnKatRashodi.Name = "rbtnKatRashodi";
+            this.rbtnKatRashodi.Size = new System.Drawing.Size(67, 19);
+            this.rbtnKatRashodi.TabIndex = 6;
+            this.rbtnKatRashodi.TabStop = true;
+            this.rbtnKatRashodi.Text = "Rashodi";
+            this.rbtnKatRashodi.UseVisualStyleBackColor = true;
+            // 
+            // btnIzbrisiKat
+            // 
+            this.btnIzbrisiKat.Location = new System.Drawing.Point(12, 397);
+            this.btnIzbrisiKat.Name = "btnIzbrisiKat";
+            this.btnIzbrisiKat.Size = new System.Drawing.Size(75, 41);
+            this.btnIzbrisiKat.TabIndex = 7;
+            this.btnIzbrisiKat.Text = "Izbriši kategoriju";
+            this.btnIzbrisiKat.UseVisualStyleBackColor = true;
+            this.btnIzbrisiKat.Click += new System.EventHandler(this.btnIzbrisiKat_Click);
+            // 
+            // btnDodajKat
+            // 
+            this.btnDodajKat.Location = new System.Drawing.Point(160, 397);
+            this.btnDodajKat.Name = "btnDodajKat";
+            this.btnDodajKat.Size = new System.Drawing.Size(75, 41);
+            this.btnDodajKat.TabIndex = 8;
+            this.btnDodajKat.Text = "Dodaj kategoriju";
+            this.btnDodajKat.UseVisualStyleBackColor = true;
+            this.btnDodajKat.Click += new System.EventHandler(this.btnDodajKat_Click);
+            // 
+            // txtDodajKat
+            // 
+            this.txtDodajKat.Location = new System.Drawing.Point(160, 357);
+            this.txtDodajKat.Name = "txtDodajKat";
+            this.txtDodajKat.Size = new System.Drawing.Size(100, 23);
+            this.txtDodajKat.TabIndex = 9;
+            // 
             // formAnaliza
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(521, 450);
+            this.Controls.Add(this.txtDodajKat);
+            this.Controls.Add(this.btnDodajKat);
+            this.Controls.Add(this.btnIzbrisiKat);
+            this.Controls.Add(this.rbtnKatRashodi);
+            this.Controls.Add(this.rbtnKatPrihodi);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbKategorije);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cmbValutaAnaliza);
             this.Controls.Add(this.rtbAnaliza);
@@ -86,5 +167,12 @@ namespace BudzetApp
         private System.Windows.Forms.RichTextBox rtbAnaliza;
         private System.Windows.Forms.ComboBox cmbValutaAnaliza;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbKategorije;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbtnKatPrihodi;
+        private System.Windows.Forms.RadioButton rbtnKatRashodi;
+        private System.Windows.Forms.Button btnIzbrisiKat;
+        private System.Windows.Forms.Button btnDodajKat;
+        private System.Windows.Forms.TextBox txtDodajKat;
     }
 }
--- BudzetApp/BudzetApp/formAnaliza.cs
+++ BudzetApp/BudzetApp/formAnaliza.cs
@@ -50,6 +50,7 @@ namespace BudzetApp
             
             koeficijentKonverzije = 1;
             valutaAnaliza = "HRK";
+            rbtnKatPrihodi.Checked = true;
             IspisAnalize();
 
         }
@@ -70,6 +71,116 @@ namespace BudzetApp
 
             }
 
+        }
+
+        private void rbtnKatPrihodi_CheckedChanged(object sender, EventArgs e)
+        {
+            cmbKategorije.Items.Clear();
+
+            if (rbtnKatPrihodi.Checked)
+            {
+                foreach (KeyValuePair<string, double> kategorija in Form1.kategorijePrihoda)
+                {
+                    cmbKategorije.Items.Add(kategorija.Key);
+                }
+            }
+            else
+            {
+                foreach (KeyValuePair<string, double> kategorija in Form1.kategorijeRashoda)
+                {
+                    cmbKategorije.Items.Add(kategorija.Key);
+                }
+            }
+        }
+
+        private void btnIzbrisiKat_Click(object sender, EventArgs e)
+        {
+            if (cmbKategorije.SelectedIndex == -1)
+            {
+                MessageBox.Show("Odaberite kategoriju za brisanje!");
+                return;
+            }
+
+            string kategorijaZaBrisanje = cmbKategorije.SelectedItem.ToString();
+
+            if (rbtnKatPrihodi.Checked)
+            {
+                if (Form1.kategorijePrihoda[kategorijaZaBrisanje] != 0)
+                {
+                    MessageBox.Show("Nije moguće izbrisati kategoriju koja nije prazna!");
+                    return;
+                }
+                else
+                {
+                    Form1.kategorijePrihoda.Remove(kategorijaZaBrisanje);
+                    MessageBox.Show("Kategorija uspješno izbrisana!");
+                }
+            }
+            else
+            {
+                if (Form1.kategorijeRashoda[kategorijaZaBrisanje] != 0)
+                {
+                    MessageBox.Show("Nije moguće izbrisati kategoriju koja nije prazna!");
+                    return;
+                }
+                else
+                {
+                    Form1.kategorijeRashoda.Remove(kategorijaZaBrisanje);
+                    MessageBox.Show("Kategorija uspješno izbrisana!");
+                }
+            }
+
+            rbtnKatPrihodi.Checked = false;
+            rbtnKatPrihodi.Checked = true;
+            cmbKategorije.Text = "";
+            txtDodajKat.Text = "";
+            IspisAnalize();
+
+
+        }
+
+        private void btnDodajKat_Click(object sender, EventArgs e)
+        {
+            if (txtDodajKat.Text == "")
+            {
+                MessageBox.Show("Napišite ime kategorije koju želite dodati");
+                return;
+            }
+
+            string kategorijaZaDodati = txtDodajKat.Text;
+
+            if (rbtnKatPrihodi.Checked)
+            {
+                if (Form1.kategorijePrihoda.ContainsKey(kategorijaZaDodati))
+                {
+                    MessageBox.Show("Kategorija već postoji!");
+                    return;
+                }
+                else
+                {
+                    Form1.kategorijePrihoda.Add(kategorijaZaDodati, 0);
+                    MessageBox.Show("Kategorija uspješno dodana!");
+                }
+            }
+            else
+            {
+                if (Form1.kategorijeRashoda.ContainsKey(kategorijaZaDodati))
+                {
+                    MessageBox.Show("Kategorija već postoji!");
+                    return;
+                }
+                else
+                {
+                    Form1.kategorijeRashoda.Add(kategorijaZaDodati, 0);
+                    MessageBox.Show("Kategorija uspješno dodana!");
+                }
+            }
+
+            rbtnKatPrihodi.Checked = false;
+            rbtnKatPrihodi.Checked = true;
+            cmbKategorije.Text = "";
+            txtDodajKat.Text = "";
+            IspisAnalize();
         }
     }
 }
